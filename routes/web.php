7f250203ba19cf9267a1eb63270f016f473804e4
tohin222 <vampirehaunted24@gmail.com>
2020-01-17 22:29:20 +0600

    welcome page added

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,13 @@
 |
 */
 
-Route::get('/', 'frontEndController@banner')->name('banner');
-Route::get('/contact', 'frontEndController@contact')->name('contact');
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/about', 'aboutController@about')->name('about');
-Route::get('/profile', 'myuserController@profile')->name('profile');
-
-Route::get('/profile/edit', 'myuserController@edit')->name('edit');
-Route::post('/user/update', 'myuserController@update')->name('update');
-Route::get('/backend_contact', 'HomeController@backend_contact')->name('backend_contact');
-Route::get('/backend/contact/delete/{id}', 'HomeController@contact_delete')->name('contact.delete');
-
-
-Route::resource('banner', 'bannerController');
-Route::resource('header', 'headerController');
-Route::resource('client', 'clientController');
-Route::resource('team', 'teamController');
-Route::resource('portfolio', 'portfolioController');
-Route::resource('mycontact', 'contactController');
